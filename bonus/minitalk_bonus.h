c60951e063035d91cf6f8c13c289489a621bd0e5
daykim <45174177+ekdud0529@users.noreply.github.com>
2022-06-28 21:12:12 +0900

    add bonus and modify bonus error

---
+++ bonus/minitalk_bonus.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daykim <daykim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/28 21:01:49 by daykim            #+#    #+#             */
+/*   Updated: 2022/06/28 21:01:49 by daykim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
+
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include "../libft/libft.h"
+
+#endif
